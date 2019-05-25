ab17694dea190b437044a3beedc4d69c5b846f38
marko delgadillo <markoneseven@gmail.com>
2019-05-24 20:10:31 -0700

    Added mulitple templates with some test logic.

--- app.js
+++ app.js
@@ -2,12 +2,37 @@ const express = require("express");
 
 const app = express();
 
+const names = [
+  { first: null, last: "Einstein" },
+  { first: "Marko", last: "Delgadillo" },
+  { first: "Breadna", last: "Pancakes" },
+  { first: "Karen", last: "Dahmer" }
+];
+
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("<h1>There you are!</h1>");
+  // the parameter to .render is the name of the template
+  res.render("index");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<h1>Hi, JavaScript GOD!</h1>");
+// // routed to the /cards endpoint
+// app.get("/cards", (req, res) => {
+//   // rendering using the card.pug template
+//   // sending the value of prompt in the parameter to the variable of the same name in the template file
+//   res.render("card", { prompt: "Who is buried in Grant's tomb?", hint: "Think about who's tomb it is!" });
+// });
+
+// using res.locals to set the value of the variable in the template file
+app.get("/cards", (req, res) => {
+  res.locals.prompt = "Who is buried in Grant's tomb?";
+  res.locals.hint = "Think about who's tomb it is!";
+  // res.locals.names = names;
+  res.render("card");
+});
+
+app.get("/sandbox", (req, res) => {
+  res.render();
 });
 
 app.listen(1717, () => {
