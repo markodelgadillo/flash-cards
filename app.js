3189689a9ec37f2d723c04df68ad19c42d5452fb
marko delgadillo <markoneseven@gmail.com>
2019-05-25 17:28:49 -0700

    Required cookie parser.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const names = [
   { first: null, last: "Einstein" },
