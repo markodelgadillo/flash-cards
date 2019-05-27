45ed441da4235a8a7ee8aac5721a5372bf01d624
marko delgadillo <markoneseven@gmail.com>
2019-05-26 21:19:17 -0700

    Added middleware that will throw an error and one that will handle the error. Also created error.pug to display the error message.

--- app.js
+++ app.js
@@ -5,27 +5,29 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const names = [
-  { first: null, last: "Einstein" },
-  { first: "Marko", last: "Delgadillo" },
-  { first: "Breadna", last: "Pancakes" },
-  { first: "Karen", last: "Dahmer" }
-];
+// const names = [
+//   { first: null, last: "Einstein" },
+//   { first: "Marko", last: "Delgadillo" },
+//   { first: "Breadna", last: "Pancakes" },
+//   { first: "Karen", last: "Dahmer" }
+// ];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "pug");
 
 app.use((req, res, next) => {
-  req.message = "This message made it!";
-  next();
+  console.log("Hello");
+  const err = new Error("Oh No!");
+  err.status = 500;
+  next(err);
 });
 
 app.use((req, res, next) => {
-  console.log(req.message);
-  next();
+  console.log("world!");
+  next(err);
 });
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   const name = req.cookies.username;
@@ -66,9 +68,6 @@ app.post("/hello", (req, res) => {
     res.redirect("/");
   } else {
     res.render("nameError");
-    // app.post("nameError", (req, res) => {
-    //   res.redirect("/hello");
-    // });
   }
 });
 
@@ -79,6 +78,12 @@ app.post("/goodbye", (req, res) => {
 
 app.get("/sandbox", (req, res) => {
   res.render("sandbox");
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error");
 });
 
 app.listen(1717, () => {
